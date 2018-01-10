c22ace2fd53112c7866b5b35c7b47da60b545fac
Patrick Browne <pt.browne@gmail.com>
2018-01-10 13:09:08 +0100

    publish: Merge pull request #4 from cozy/update
    generated from commit da0e7e95cb1aad38a396db6f8b01414da0ae0ffa

--- index.js
+++ index.js
@@ -56057,6 +56057,10 @@ const getTotalReimbursements = operation => {
   return operation.reimbursements.reduce((s, r) => s + r.amount, 0)
 }
 
+const logMatch = (bill, operation) => {
+  console.log('Found matching ', bill, operation)
+}
+
 // DOES NOT NEED COZY CLIENT
 const findReimbursedOperation = (bill, operations, options) => {
   if (!bill.isRefund) { return null }
@@ -56182,6 +56186,10 @@ class Linker {
 
   linkMatchingOperation (bill, operations, options) {
     const matchingOp = findMatchingOperation(bill, operations, options)
+    if (matchingOp) {
+      logMatch(bill, matchingOp)
+    }
+
     if (matchingOp) {
       if (!matchingOp) { return }
       return this.addBillToOperation(bill, matchingOp).then(() => matchingOp)
@@ -102917,10 +102925,11 @@ module.exports = (entries, fields, options = {}) => {
     return entry
   }
 
+  const originalEntries = entries
   return saveFiles(entries, fields, options)
     .then(entries => filterData(entries, DOCTYPE, options))
     .then(entries => addData(entries, DOCTYPE, options))
-    .then(entries => linkBankOperations(entries, DOCTYPE, fields, options))
+    .then(entries => linkBankOperations(originalEntries, DOCTYPE, fields, options))
 }
 
 
